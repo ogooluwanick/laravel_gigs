f290fe987edb5762dc857bf8399f555bc6ce5df4
Ogooluwa Olutimilehin <ogooluwanick@gmail.com>
2022-09-08 22:11:35 +0100

    set up for layout

--- resources/views/listen.blade.php
+++ resources/views/listen.blade.php
@@ -1,2 +1,6 @@
-<h1>{{$listen["title"]}}</h1>
-<p>{{$listen["desc"]}}</p>
+@extends('Layout')
+
+@section('content')
+        <h1>{{$listen["title"]}}</h1>
+        <p>{{$listen["desc"]}}</p>
+@endsection
--- resources/views/listens.blade.php
+++ resources/views/listens.blade.php
@@ -1,9 +1,13 @@
-<h1>{{$heading}}</h1>
-        @if(count($listens)==0)
-                <p>No Lists found!</p>
-        @endif
+@extends('Layout')
 
-        @foreach($listens as $item)
-                <h2 > <a href="/listen/{{$item["id"]}}">{{$item["title"]}}</a></h2>
-                <p>{{$item["desc"]}}</p>
-        @endforeach
+@section('content')
+        <h1>{{$heading}}</h1>
+                @if(count($listens)==0)
+                        <p>No Lists found!</p>
+                @endif
+
+                @foreach($listens as $item)
+                        <h2 > <a href="/listen/{{$item["id"]}}">{{$item["title"]}}</a></h2>
+                        <p>{{$item["desc"]}}</p>
+                @endforeach
+@endsection
