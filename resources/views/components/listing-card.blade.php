08e07bcc5c074fd5e6948f7faaf19d82dfe88aac
Ogooluwa Olutimilehin <ogooluwanick@gmail.com>
2022-09-15 14:12:54 +0100

    made tag component dynamic

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -12,28 +12,7 @@
                 <a href="/listings/{{$item["id"]}}">{{$item->title}}</a>
                 </h3>
                 <div class="text-xl font-bold mb-4">{{$item["company"]}}</div>
-                <ul class="flex">
-                <li
-                        class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                        <a href="#">Laravel</a>
-                </li>
-                <li
-                        class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                        <a href="#">API</a>
-                </li>
-                <li
-                        class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                        <a href="#">Backend</a>
-                </li>
-                <li
-                        class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                        <a href="#">Vue</a>
-                </li>
-                </ul>
+                <x-listing-tags :tagsCsv="$item->tags"/>
                 <div class="text-lg mt-4">
                 <i class="fa-solid fa-location-dot"></i> {{$item->location}}
                 </div>
