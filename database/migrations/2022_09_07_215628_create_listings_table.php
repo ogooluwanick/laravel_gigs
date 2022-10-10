bdaf7a1cc4c17776a553bf238f361d3962b3c9c8
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2022-10-10 21:47:48 +0100

    added relationship to user, listing in Models/migrations/seeders

--- database/migrations/2022_09_07_215628_create_listings_table.php
+++ database/migrations/2022_09_07_215628_create_listings_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('listings', function (Blueprint $table) {
             $table->id();
+            $table->foreignID("user_id")->constrained()->onDelete("cascade");
             $table->string("title");
             $table->string("tags");
             $table->string("company");
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
@@ -15,27 +16,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(5)->create();
-        Listing::factory(4)->create();
+        // \App\Models\User::factory(5)->create();
+        $user=User::factory()->create(["name"=>"John Smart","email"=>"John@mail.com"]);;
+        Listing::factory(4)->create(["user_id"=>$user->id]);
 
-        Listing::create([
-                'title'=>'Laravel Snr Dev',
-                "tags"=>"laravel,js",
-                "company"=>"ACME corp",
-                "location"=>"Lagos, Nig",
-                "email"=>"acme.mai@lmail.com",
-                "website"=>"www.acme.co.uk",
-                "desc"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. A, explicabo minus? Ullam culpa architecto dolorem, quam dolor quibusdam odit ut neque non incidunt possimus a deserunt debitis libero. Voluptatibus, est?",
-        ]);
-        Listing::create([
-                'title'=>'Full stack Dev',
-                "tags"=>"MERN,js,react,api",
-                "company"=>"Steark corp",
-                "location"=>"ABJ, Nig",
-                "email"=>"starkdumb@fmail.com",
-                "website"=>"www.stack.co.mk",
-                "desc"=>"FUorem mmmm dolor sit amet consectetur sure check me tf  elit. A, explicabo minus? Ullam culpa architecto dolorem, quam dolor quibusdam odit ut neque non incidunt possimus a deserunt debitis libero. Voluptatibus, est?",
-        ]);
+        // Listing::create([
+        //         'title'=>'Laravel Snr Dev',
+        //         "tags"=>"laravel,js",
+        //         "company"=>"ACME corp",
+        //         "location"=>"Lagos, Nig",
+        //         "email"=>"acme.mai@lmail.com",
+        //         "website"=>"www.acme.co.uk",
+        //         "desc"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. A, explicabo minus? Ullam culpa architecto dolorem, quam dolor quibusdam odit ut neque non incidunt possimus a deserunt debitis libero. Voluptatibus, est?",
+        // ]);
+        // Listing::create([
+        //         'title'=>'Full stack Dev',
+        //         "tags"=>"MERN,js,react,api",
+        //         "company"=>"Steark corp",
+        //         "location"=>"ABJ, Nig",
+        //         "email"=>"starkdumb@fmail.com",
+        //         "website"=>"www.stack.co.mk",
+        //         "desc"=>"FUorem mmmm dolor sit amet consectetur sure check me tf  elit. A, explicabo minus? Ullam culpa architecto dolorem, quam dolor quibusdam odit ut neque non incidunt possimus a deserunt debitis libero. Voluptatibus, est?",
+        // ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
